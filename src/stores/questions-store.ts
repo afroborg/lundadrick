c15704fa841bd7d577d09e23aa0a3d0ffae70c15
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2021-09-13 17:03:23 +0200

    fix warnings

--- src/stores/questions-store.ts
+++ src/stores/questions-store.ts
@@ -3,12 +3,12 @@ import { writable } from 'svelte/store';
 export const questions = writable<string[]>([]);
 export const totalNbrOfQuestions = writable<number>(0);
 
-export const setQuestions = (qs: string[]) => {
+export const setQuestions = (qs: string[]): void => {
 	questions.set(qs);
 	totalNbrOfQuestions.set(qs.length);
 };
 
-export const nextQuestion = (curr: string) => {
+export const nextQuestion = (curr: string): null => {
 	questions.update((q) => {
 		return q.filter((question) => question !== curr);
 	});
